9ebe330ae382a23bf82b59e4335be2b831829316
Михаил <dontsov.mih@yandex.ru>
2013-12-21 16:38:04 +0000

    Imported from Changeset 13

--- Лаба ООП 3/Лаба ООП 3/Form1.Designer.cs
+++ Лаба ООП 3/Лаба ООП 3/Form1.Designer.cs
@@ -30,19 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip_Main = new System.Windows.Forms.MenuStrip();
-            this.statusStrip_AllStat = new System.Windows.Forms.StatusStrip();
             this.toolStripMenuItem_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_ExportStat = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_Modeling = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemModelSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_Service = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip_AllStat = new System.Windows.Forms.StatusStrip();
             this.groupBox_RealTimeModel = new System.Windows.Forms.GroupBox();
             this.listBox_ModelStations = new System.Windows.Forms.ListBox();
             this.toolStrip_MainTools = new System.Windows.Forms.ToolStrip();
-            this.toolStripMenuItem_Modeling = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripButton_Start = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton_Stop = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripMenuItem_Service = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem_Help = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItemModelSettings = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem_ExportStat = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip_Main.SuspendLayout();
             this.groupBox_RealTimeModel.SuspendLayout();
             this.toolStrip_MainTools.SuspendLayout();
@@ -56,32 +56,70 @@
             this.toolStripMenuItem_Service});
             this.menuStrip_Main.Location = new System.Drawing.Point(0, 0);
             this.menuStrip_Main.Name = "menuStrip_Main";
-            this.menuStrip_Main.Size = new System.Drawing.Size(733, 28);
+            this.menuStrip_Main.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip_Main.Size = new System.Drawing.Size(550, 24);
             this.menuStrip_Main.TabIndex = 0;
             this.menuStrip_Main.Text = "Главное меню";
-            // 
-            // statusStrip_AllStat
-            // 
-            this.statusStrip_AllStat.Location = new System.Drawing.Point(0, 460);
-            this.statusStrip_AllStat.Name = "statusStrip_AllStat";
-            this.statusStrip_AllStat.Size = new System.Drawing.Size(733, 22);
-            this.statusStrip_AllStat.TabIndex = 1;
-            this.statusStrip_AllStat.Text = "Статус исполнения";
             // 
             // toolStripMenuItem_File
             // 
             this.toolStripMenuItem_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem_ExportStat});
             this.toolStripMenuItem_File.Name = "toolStripMenuItem_File";
-            this.toolStripMenuItem_File.Size = new System.Drawing.Size(57, 24);
+            this.toolStripMenuItem_File.Size = new System.Drawing.Size(48, 20);
             this.toolStripMenuItem_File.Text = "Файл";
+            // 
+            // toolStripMenuItem_ExportStat
+            // 
+            this.toolStripMenuItem_ExportStat.Name = "toolStripMenuItem_ExportStat";
+            this.toolStripMenuItem_ExportStat.Size = new System.Drawing.Size(194, 22);
+            this.toolStripMenuItem_ExportStat.Text = "Сохранить статистику";
+            // 
+            // toolStripMenuItem_Modeling
+            // 
+            this.toolStripMenuItem_Modeling.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemModelSettings});
+            this.toolStripMenuItem_Modeling.Name = "toolStripMenuItem_Modeling";
+            this.toolStripMenuItem_Modeling.Size = new System.Drawing.Size(109, 20);
+            this.toolStripMenuItem_Modeling.Text = "Моделирование";
+            // 
+            // toolStripMenuItemModelSettings
+            // 
+            this.toolStripMenuItemModelSettings.Name = "toolStripMenuItemModelSettings";
+            this.toolStripMenuItemModelSettings.Size = new System.Drawing.Size(223, 22);
+            this.toolStripMenuItemModelSettings.Text = "Настроить моделирование";
+            // 
+            // toolStripMenuItem_Service
+            // 
+            this.toolStripMenuItem_Service.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_Help});
+            this.toolStripMenuItem_Service.Name = "toolStripMenuItem_Service";
+            this.toolStripMenuItem_Service.Size = new System.Drawing.Size(59, 20);
+            this.toolStripMenuItem_Service.Text = "Сервис";
+            // 
+            // toolStripMenuItem_Help
+            // 
+            this.toolStripMenuItem_Help.Name = "toolStripMenuItem_Help";
+            this.toolStripMenuItem_Help.Size = new System.Drawing.Size(120, 22);
+            this.toolStripMenuItem_Help.Text = "Справка";
+            // 
+            // statusStrip_AllStat
+            // 
+            this.statusStrip_AllStat.Location = new System.Drawing.Point(0, 370);
+            this.statusStrip_AllStat.Name = "statusStrip_AllStat";
+            this.statusStrip_AllStat.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip_AllStat.Size = new System.Drawing.Size(550, 22);
+            this.statusStrip_AllStat.TabIndex = 1;
+            this.statusStrip_AllStat.Text = "Статус исполнения";
             // 
             // groupBox_RealTimeModel
             // 
             this.groupBox_RealTimeModel.Controls.Add(this.listBox_ModelStations);
-            this.groupBox_RealTimeModel.Location = new System.Drawing.Point(12, 56);
+            this.groupBox_RealTimeModel.Location = new System.Drawing.Point(9, 46);
+            this.groupBox_RealTimeModel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox_RealTimeModel.Name = "groupBox_RealTimeModel";
-            this.groupBox_RealTimeModel.Size = new System.Drawing.Size(347, 401);
+            this.groupBox_RealTimeModel.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox_RealTimeModel.Size = new System.Drawing.Size(260, 326);
             this.groupBox_RealTimeModel.TabIndex = 2;
             this.groupBox_RealTimeModel.TabStop = false;
             this.groupBox_RealTimeModel.Text = "Модель в реальном времени";
@@ -89,10 +127,10 @@
             // listBox_ModelStations
             // 
             this.listBox_ModelStations.FormattingEnabled = true;
-            this.listBox_ModelStations.ItemHeight = 16;
-            this.listBox_ModelStations.Location = new System.Drawing.Point(6, 17);
+            this.listBox_ModelStations.Location = new System.Drawing.Point(4, 14);
+            this.listBox_ModelStations.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox_ModelStations.Name = "listBox_ModelStations";
-            this.listBox_ModelStations.Size = new System.Drawing.Size(335, 372);
+            this.listBox_ModelStations.Size = new System.Drawing.Size(252, 303);
             this.listBox_ModelStations.TabIndex = 0;
             // 
             // toolStrip_MainTools
@@ -101,19 +139,11 @@
             this.toolStripButton_Start,
             this.toolStripButton_Stop,
             this.toolStripSeparator1});
-            this.toolStrip_MainTools.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip_MainTools.Location = new System.Drawing.Point(0, 24);
             this.toolStrip_MainTools.Name = "toolStrip_MainTools";
-            this.toolStrip_MainTools.Size = new System.Drawing.Size(733, 25);
+            this.toolStrip_MainTools.Size = new System.Drawing.Size(550, 25);
             this.toolStrip_MainTools.TabIndex = 3;
             this.toolStrip_MainTools.Text = "Инструменты";
-            // 
-            // toolStripMenuItem_Modeling
-            // 
-            this.toolStripMenuItem_Modeling.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItemModelSettings});
-            this.toolStripMenuItem_Modeling.Name = "toolStripMenuItem_Modeling";
-            this.toolStripMenuItem_Modeling.Size = new System.Drawing.Size(136, 24);
-            this.toolStripMenuItem_Modeling.Text = "Моделирование";
             // 
             // toolStripButton_Start
             // 
@@ -138,37 +168,11 @@
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
-            // toolStripMenuItem_Service
-            // 
-            this.toolStripMenuItem_Service.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem_Help});
-            this.toolStripMenuItem_Service.Name = "toolStripMenuItem_Service";
-            this.toolStripMenuItem_Service.Size = new System.Drawing.Size(71, 24);
-            this.toolStripMenuItem_Service.Text = "Сервис";
-            // 
-            // toolStripMenuItem_Help
-            // 
-            this.toolStripMenuItem_Help.Name = "toolStripMenuItem_Help";
-            this.toolStripMenuItem_Help.Size = new System.Drawing.Size(175, 24);
-            this.toolStripMenuItem_Help.Text = "Справка";
-            // 
-            // toolStripMenuItemModelSettings
-            // 
-            this.toolStripMenuItemModelSettings.Name = "toolStripMenuItemModelSettings";
-            this.toolStripMenuItemModelSettings.Size = new System.Drawing.Size(268, 24);
-            this.toolStripMenuItemModelSettings.Text = "Настроить моделирование";
-            // 
-            // toolStripMenuItem_ExportStat
-            // 
-            this.toolStripMenuItem_ExportStat.Name = "toolStripMenuItem_ExportStat";
-            this.toolStripMenuItem_ExportStat.Size = new System.Drawing.Size(228, 24);
-            this.toolStripMenuItem_ExportStat.Text = "Сохранить статистику";
-            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(733, 482);
+            this.ClientSize = new System.Drawing.Size(550, 392);
             this.Controls.Add(this.toolStrip_MainTools);
             this.Controls.Add(this.groupBox_RealTimeModel);
             this.Controls.Add(this.statusStrip_AllStat);
@@ -176,7 +180,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.HelpButton = true;
             this.MainMenuStrip = this.menuStrip_Main;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.ShowIcon = false;
