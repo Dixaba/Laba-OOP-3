01879f1e2e329074d6af32626d46e958cb799282
Кругликов Арсений Дмитриевич <kramer98489@hotmail.com>
2013-12-22 21:54:36 +0000

    Imported from Changeset 40

--- Лаба ООП 3/Лаба ООП 3/Statistics.cs
+++ Лаба ООП 3/Лаба ООП 3/Statistics.cs
@@ -12,7 +12,21 @@ namespace Лаба_ООП_3
 
         #region
 
-        
+        Double AvgRideLength { get; set; }
+
+        Double AvgWaitingTime { get; set; }
+
+        Double AvgQueueLenghtAtPeak { get; set; }
+
+        UInt32 MaxQueueLenght { get; set; }
+
+        UInt32 MaxQueueTime { get; set; }
+
+        UInt32 MaxQueueStation { get; set; }
+
+        UInt32 Count { get; set; }
+
+        UInt32 ServedCount { get; set; }
 
         #endregion
     }
