026b6ff79e4a94e470767cbe0e0c33f63b6e0de6
Михаил <dontsov.mih@yandex.ru>
2013-12-22 19:37:18 +0000

    Imported from Changeset 28

---
+++ Лаба ООП 3/Лаба ООП 3/Poisson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_ООП_3
+{
+    static class Poisson
+    {
+        static double AntiFact(double a, uint x)
+        {
+            if (x <= 1) return a;
+
+            double res = a;
+            for (UInt64 i = 2; i <= x; i++)
+                res /= i;
+            return res;
+        }
+        public static UInt32 Get(UInt32 IntervalNumber, UInt32 PassengerCount, double Lambda, UInt32 StationCount)
+        {
+            double m = IntervalNumber * 1.0;
+            
+            Random RNG = new Random();
+
+            double res = Math.Pow(Lambda, m) * Math.Exp(-Lambda) * PassengerCount * 2.0 * RNG.NextDouble() / StationCount;
+
+            return (UInt32)Math.Round(res);
+        }
+    }
+}
