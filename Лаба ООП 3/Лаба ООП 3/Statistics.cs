2886e19e09d6bb65db71a9a8ecb5b6a54db84e1f
Кругликов Арсений Дмитриевич <kramer98489@hotmail.com>
2013-12-22 21:30:59 +0000

    Imported from Changeset 39

---
+++ Лаба ООП 3/Лаба ООП 3/Statistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_ООП_3
+{
+    class Statistics
+    {
+        List<Passenger> ServedPassengers;
+
+        #region
+
+        
+
+        #endregion
+    }
+}
