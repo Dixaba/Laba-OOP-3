340cdc4b245b6baa0ad97c1cf3b56e493e9106cf
Кругликов Арсений Дмитриевич <kramer98489@hotmail.com>
2013-12-22 19:22:22 +0000

    Imported from Changeset 25

---
+++ Лаба ООП 3/Лаба ООП 3/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Лаба_ООП_3 
+{
+    public class Passenger   //Тельнов мудак
+    {
+        #region Свойства. и к тому же тельнов мудак
+        
+        UInt32 IncomingTime { get; set; }
+
+        UInt32 ArrivingTime { get; set; }
+
+        UInt32 IncomingStation { get; set; }
+
+        UInt32 ArrivingStation { get; set; }
+
+        UInt32 ArrivingStation2 { get; set; }
+
+        #endregion
+    }
+}
