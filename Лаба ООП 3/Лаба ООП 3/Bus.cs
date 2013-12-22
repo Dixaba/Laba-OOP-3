79830fd66cfc2e901c6b2e1ffd0720f9c7316f1e
Кругликов Арсений Дмитриевич <kramer98489@hotmail.com>
2013-12-22 20:33:00 +0000

    Imported from Changeset 38

---
+++ Лаба ООП 3/Лаба ООП 3/Bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_ООП_3
+{
+    public class Bus
+    {
+        List<Passenger> BusContent;
+
+        public Bus(UInt32 capacity)
+        {
+            BusCapacity = capacity;
+            BusContent = new List<Passenger>((int)BusCapacity);
+        }
+
+        #region Свойства
+
+        UInt32 CurrentStation { get; set; }
+
+        public UInt32 RidingTime { get; set; }
+
+        public UInt32 BusCapacity { get; set; }
+
+        #endregion
+    }
+}
