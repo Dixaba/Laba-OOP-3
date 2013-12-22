53f0695f5d4618d7db9f2634899efee65df70060
Кругликов Арсений Дмитриевич <kramer98489@hotmail.com>
2013-12-22 19:50:14 +0000

    Imported from Changeset 31

---
+++ Лаба ООП 3/Лаба ООП 3/CircularRoute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_ООП_3
+{
+    public class CircularRoute
+    {
+        Queue<Passenger>[] Stations;
+
+        public
+    }
+}
