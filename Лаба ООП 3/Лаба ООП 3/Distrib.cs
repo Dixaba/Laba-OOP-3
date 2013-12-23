1b86299e01b3b21866a904496cbbf87bf9fb9469
Михаил <dontsov.mih@yandex.ru>
2013-12-23 18:04:21 +0000

    Distrib class released Imported from Changeset 43

---
+++ Лаба ООП 3/Лаба ООП 3/Distrib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Meta.Numerics.Statistics.Distributions;
+
+namespace Лаба_ООП_3
+{
+    static class Distrib
+    {
+        public static uint Get(uint PassengerCount, double PeakTime, double PeakWidth, uint Minute, uint StationCount)
+        {
+            Random RNG = new Random();
+
+            LogisticDistribution logis = new LogisticDistribution(PeakTime, PeakWidth);
+
+            return (uint)Math.Round(PassengerCount * logis.ProbabilityDensity(Minute) * 2.0 * RNG.NextDouble()/StationCount);
+        }
+    }
+}
