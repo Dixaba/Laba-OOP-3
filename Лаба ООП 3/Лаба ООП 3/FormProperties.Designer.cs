42b2f777a4bd202ff7336f91211f483a1d3630b6
Михаил <dontsov.mih@yandex.ru>
2013-12-23 19:42:12 +0000

    Imported from Changeset 55

--- Лаба ООП 3/Лаба ООП 3/FormProperties.Designer.cs
+++ Лаба ООП 3/Лаба ООП 3/FormProperties.Designer.cs
@@ -64,7 +64,7 @@
             // 
             this.button_Accept.AutoSize = true;
             this.button_Accept.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button_Accept.Location = new System.Drawing.Point(295, 267);
+            this.button_Accept.Location = new System.Drawing.Point(212, 267);
             this.button_Accept.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_Accept.Name = "button_Accept";
             this.button_Accept.Size = new System.Drawing.Size(99, 28);
@@ -77,7 +77,7 @@
             // 
             this.button_Cancel.AutoSize = true;
             this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button_Cancel.Location = new System.Drawing.Point(200, 267);
+            this.button_Cancel.Location = new System.Drawing.Point(117, 267);
             this.button_Cancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_Cancel.Name = "button_Cancel";
             this.button_Cancel.Size = new System.Drawing.Size(89, 28);
@@ -109,7 +109,7 @@
             this.groupBox_Properties.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox_Properties.Name = "groupBox_Properties";
             this.groupBox_Properties.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox_Properties.Size = new System.Drawing.Size(381, 249);
+            this.groupBox_Properties.Size = new System.Drawing.Size(308, 249);
             this.groupBox_Properties.TabIndex = 2;
             this.groupBox_Properties.TabStop = false;
             this.groupBox_Properties.Text = "Настройки";
@@ -117,33 +117,33 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(241, 190);
+            this.label10.Location = new System.Drawing.Point(178, 189);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(33, 17);
+            this.label10.Size = new System.Drawing.Size(27, 13);
             this.label10.TabIndex = 18;
             this.label10.Text = "Час";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(217, 218);
+            this.label9.Location = new System.Drawing.Point(161, 217);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(57, 17);
+            this.label9.Size = new System.Drawing.Size(44, 13);
             this.label9.TabIndex = 17;
             this.label9.Text = "Минута";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(145, 190);
+            this.label8.Location = new System.Drawing.Point(83, 202);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(90, 17);
+            this.label8.Size = new System.Drawing.Size(71, 13);
             this.label8.TabIndex = 16;
             this.label8.Text = "Час пик №2:";
             // 
             // numericUpDown_Min2
             // 
-            this.numericUpDown_Min2.Location = new System.Drawing.Point(281, 214);
+            this.numericUpDown_Min2.Location = new System.Drawing.Point(210, 215);
             this.numericUpDown_Min2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Min2.Maximum = new decimal(new int[] {
             59,
@@ -151,12 +151,12 @@
             0,
             0});
             this.numericUpDown_Min2.Name = "numericUpDown_Min2";
-            this.numericUpDown_Min2.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Min2.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Min2.TabIndex = 15;
             // 
             // numericUpDown_Hour2
             // 
-            this.numericUpDown_Hour2.Location = new System.Drawing.Point(281, 187);
+            this.numericUpDown_Hour2.Location = new System.Drawing.Point(210, 188);
             this.numericUpDown_Hour2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Hour2.Maximum = new decimal(new int[] {
             23,
@@ -164,7 +164,7 @@
             0,
             0});
             this.numericUpDown_Hour2.Name = "numericUpDown_Hour2";
-            this.numericUpDown_Hour2.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Hour2.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Hour2.TabIndex = 14;
             this.numericUpDown_Hour2.Value = new decimal(new int[] {
             12,
@@ -175,18 +175,18 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(217, 162);
+            this.label7.Location = new System.Drawing.Point(161, 161);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(57, 17);
+            this.label7.Size = new System.Drawing.Size(44, 13);
             this.label7.TabIndex = 13;
             this.label7.Text = "Минута";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(241, 134);
+            this.label6.Location = new System.Drawing.Point(178, 133);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(33, 17);
+            this.label6.Size = new System.Drawing.Size(27, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "Час";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -194,15 +194,15 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(145, 132);
+            this.label5.Location = new System.Drawing.Point(82, 145);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.Size = new System.Drawing.Size(71, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "Час пик №1:";
             // 
             // numericUpDown_Min1
             // 
-            this.numericUpDown_Min1.Location = new System.Drawing.Point(281, 160);
+            this.numericUpDown_Min1.Location = new System.Drawing.Point(210, 161);
             this.numericUpDown_Min1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Min1.Maximum = new decimal(new int[] {
             59,
@@ -210,48 +210,48 @@
             0,
             0});
             this.numericUpDown_Min1.Name = "numericUpDown_Min1";
-            this.numericUpDown_Min1.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Min1.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Min1.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(67, 105);
+            this.label4.Location = new System.Drawing.Point(42, 104);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(207, 17);
+            this.label4.Size = new System.Drawing.Size(163, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Вместимость маршрутного ТС";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 76);
+            this.label3.Location = new System.Drawing.Point(6, 75);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(252, 17);
+            this.label3.Size = new System.Drawing.Size(199, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Время движения между остановками";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 48);
+            this.label2.Location = new System.Drawing.Point(34, 47);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(219, 17);
+            this.label2.Size = new System.Drawing.Size(171, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "Количество пассажиров в сутки";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 18);
+            this.label1.Location = new System.Drawing.Point(83, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 17);
+            this.label1.Size = new System.Drawing.Size(122, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "Количество остановок";
             // 
             // numericUpDown_Hour1
             // 
-            this.numericUpDown_Hour1.Location = new System.Drawing.Point(281, 130);
+            this.numericUpDown_Hour1.Location = new System.Drawing.Point(210, 131);
             this.numericUpDown_Hour1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Hour1.Maximum = new decimal(new int[] {
             23,
@@ -259,7 +259,7 @@
             0,
             0});
             this.numericUpDown_Hour1.Name = "numericUpDown_Hour1";
-            this.numericUpDown_Hour1.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Hour1.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Hour1.TabIndex = 4;
             this.numericUpDown_Hour1.Value = new decimal(new int[] {
             12,
@@ -269,7 +269,7 @@
             // 
             // numericUpDown_Capacity
             // 
-            this.numericUpDown_Capacity.Location = new System.Drawing.Point(281, 101);
+            this.numericUpDown_Capacity.Location = new System.Drawing.Point(210, 102);
             this.numericUpDown_Capacity.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Capacity.Maximum = new decimal(new int[] {
             25,
@@ -282,7 +282,7 @@
             0,
             0});
             this.numericUpDown_Capacity.Name = "numericUpDown_Capacity";
-            this.numericUpDown_Capacity.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Capacity.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Capacity.TabIndex = 3;
             this.numericUpDown_Capacity.Value = new decimal(new int[] {
             13,
@@ -292,7 +292,7 @@
             // 
             // numericUpDown_Time
             // 
-            this.numericUpDown_Time.Location = new System.Drawing.Point(281, 73);
+            this.numericUpDown_Time.Location = new System.Drawing.Point(210, 74);
             this.numericUpDown_Time.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Time.Maximum = new decimal(new int[] {
             10,
@@ -305,7 +305,7 @@
             0,
             0});
             this.numericUpDown_Time.Name = "numericUpDown_Time";
-            this.numericUpDown_Time.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Time.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Time.TabIndex = 2;
             this.numericUpDown_Time.Value = new decimal(new int[] {
             5,
@@ -315,7 +315,7 @@
             // 
             // numericUpDown_Passangers
             // 
-            this.numericUpDown_Passangers.Location = new System.Drawing.Point(281, 44);
+            this.numericUpDown_Passangers.Location = new System.Drawing.Point(210, 45);
             this.numericUpDown_Passangers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Passangers.Maximum = new decimal(new int[] {
             50000,
@@ -328,7 +328,7 @@
             0,
             0});
             this.numericUpDown_Passangers.Name = "numericUpDown_Passangers";
-            this.numericUpDown_Passangers.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Passangers.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Passangers.TabIndex = 1;
             this.numericUpDown_Passangers.Value = new decimal(new int[] {
             10000,
@@ -338,7 +338,7 @@
             // 
             // numericUpDown_Stations
             // 
-            this.numericUpDown_Stations.Location = new System.Drawing.Point(281, 16);
+            this.numericUpDown_Stations.Location = new System.Drawing.Point(210, 17);
             this.numericUpDown_Stations.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown_Stations.Maximum = new decimal(new int[] {
             20,
@@ -351,7 +351,7 @@
             0,
             0});
             this.numericUpDown_Stations.Name = "numericUpDown_Stations";
-            this.numericUpDown_Stations.Size = new System.Drawing.Size(88, 22);
+            this.numericUpDown_Stations.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown_Stations.TabIndex = 0;
             this.numericUpDown_Stations.Value = new decimal(new int[] {
             10,
@@ -364,7 +364,7 @@
             this.AcceptButton = this.button_Accept;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.CancelButton = this.button_Cancel;
-            this.ClientSize = new System.Drawing.Size(403, 302);
+            this.ClientSize = new System.Drawing.Size(327, 302);
             this.ControlBox = false;
             this.Controls.Add(this.groupBox_Properties);
             this.Controls.Add(this.button_Cancel);
